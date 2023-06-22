906d395cd39a80565dcae051f8f525b60bb20400
isavo <isavo@DESKTOP-C4E4COM>
2023-06-22 00:29:37 -0300

    Adicionar arquivos de projeto.

---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DevFreela.API.Models;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        // api/users/1
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        // api/users
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateUserModel createUser)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = 1 }, createUser);
+        }
+
+        // api/users/1/login
+        [HttpPut("{id}/login")]
+        public IActionResult Login(int id, [FromBody] LoginModel login)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ DevFreela.Core/Entities/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities
+{
+    public class Skill
+    {
+        public string? Description { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+    }
+}
