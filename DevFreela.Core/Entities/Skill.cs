f76ff1cd6e62d34fb8666c4bc2c81dc2e43fdd86
Isabella Volotao <isabellavolotao@gmail.com>
2023-06-22 01:16:24 -0300

    Adding Core with entities, enums, exceptions and repositories.

--- DevFreela.Core/Entities/Skill.cs
+++ DevFreela.Core/Entities/Skill.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DevFreela.Core.Entities
+﻿namespace DevFreela.Core.Entities
 {
-    public class Skill
+    public class Skill : BaseEntity
     {
+        public Skill(string description)
+        {
+            Description = description;
+            CreatedAt = DateTime.Now;
+        }
+
         public string? Description { get; private set; }
         public DateTime CreatedAt { get; private set; }
     }
