8841e1f92a13bc43279ea14b03bb1081e73ed9de
Isabella Volotao <isabellavolotao@gmail.com>
2023-06-25 16:32:39 -0300

    Implementing ProjectService.

--- DevFreela.Application/Services/Implementations/ProjectService.cs
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DevFreela.Application.InputModels;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Application.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,46 @@ using System.Threading.Tasks;
 
 namespace DevFreela.Application.Services.Implementations
 {
-    public class ProjectService
+    public class ProjectService : IProjectService
     {
+        public int Create(NewProjectInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateComment(CreateCommentInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Finish(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProjectViewModel> GetAll(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProjectDetailsViewModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(UpdateProjectInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
