54d402d3dac4a34de0e6b0e9c0224e67e469a567
Isabella Volotao <isabellavolotao@gmail.com>
2023-06-26 23:43:31 -0300

    Refactoring DbContext, adding Configurations.

---
+++ DevFreela.Infrastructure/Persistence/Configurations/UserSkillConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infrastructure.Persistence.Configurations
+{
+    public class UserSkillConfigurations : IEntityTypeConfiguration<UserSkill>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<UserSkill> builder)
+        {
+            builder
+                .ToTable("tb_userskill")
+                .HasKey(u => u.Id);
+        }
+    }
+}
--- DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -1,37 +1,27 @@
 ﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace DevFreela.Infrastructure.Persistence
 {
-    public class DevFreelaDbContext
+    public class DevFreelaDbContext : DbContext
     {
-        public DevFreelaDbContext()
+        public DevFreelaDbContext(DbContextOptions<DevFreelaDbContext> options) : base(options)
         {
-            Projects = new List<Project>
-            {
-                new Project("ASPNET Core project", "Description of project", 1, 1, 10000),
-                new Project("Another ASPNET Core project", "Antoher description of project", 1, 1, 20000),
-                new Project("Last ASPNET Core project", "Last description of project", 1, 1, 30000)
-            };
-
-            Users = new List<User>
-            {
-                new User("Isabella Teste 1", "isabella1@teste.com.br", new DateTime(2000, 1, 3)),
-                new User("Isabella Teste 2", "isabella2@teste.com.br", new DateTime(1999, 2, 2)),
-                new User("Isabella Teste 3", "isabella3@teste.com.br", new DateTime(2001, 3, 1))
-            };
-
-            Skills = new List<Skill>
-            {
-                new Skill(".NET Core"),
-                new Skill("C#"),
-                new Skill("SQL")
-            };
+            
         }
 
-        public List<Project> Projects { get; set; }
-        public List<User> Users { get; set; }
-        public List<Skill> Skills { get; set; }
-        public List<ProjectComment> ProjectComments { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<UserSkill> UserSkill { get; set; }
+        public DbSet<ProjectComment> ProjectComments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
     }
 }
